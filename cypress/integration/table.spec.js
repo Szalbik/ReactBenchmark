7ac8bb6751dfd573b8e29ca527b6a3f68b2f562e
Damian Szalbierz <szalbierz.d.k@gmail.com>
2019-01-16 00:59:56 +0100

    Fix table spec

--- cypress/integration/table.spec.js
+++ cypress/integration/table.spec.js
@@ -3,12 +3,12 @@ describe("Main page", function() {
     cy.server();
     cy.route("GET", "https://jsonplaceholder.typicode.com/posts", "fixture:posts");
 
-    cy.visit("localhost:300/table");
-    cy.contains("Add 1000 rows").click();
+    cy.visit("localhost:3000");
+    cy.contains("Add 1000 Rows").click();
     cy.get("table tbody tr").should("have.length", 1000);
-    cy.contains("Update rows").click();
-    cy.contains("Swap rows").click();
-    cy.contains("Clear rows").click();
+    cy.contains("Update Rows").click();
+    cy.contains("Swap Rows").click();
+    cy.contains("Clear Rows").click();
     cy.get("table tbody tr").should("have.length", 0);
   });
 });
