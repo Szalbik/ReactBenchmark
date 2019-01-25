070b7431c0b34d8eaaea9fdee96ad22583cccda3
Damian Szalbierz <szalbierz.d.k@gmail.com>
2019-01-25 13:02:12 +0100

    bug

--- src/Table.js
+++ src/Table.js
@@ -1,17 +1,19 @@
 import React, { Component } from "react";
 import axios from 'axios';
 import _ from 'lodash';
-import jm from 'js-meter';
 import './Table.css';
 
 class Table extends Component {
   state = {
-    data: { posts: [], comments: []},
+    data: { posts: [], comments: [] },
+    posts: false,
     jsMeterOpt: {
       isPrint: true,
       isKb: true
     }
   }
+
+  posts = []
 
   constructor(props) {
     super(props);
@@ -22,86 +24,93 @@ class Table extends Component {
     this.updateEveryRow = this.updateEveryRow.bind(this)
     this.swapRows = this.swapRows.bind(this)
   }
- 
-  componentWillUpdate() {
-  }
 
   componentDidUpdate() {
     this.end = +new Date()
     console.log(`Execution Time: ${this.end - this.start} ms`)
-
   }
 
-  componentWillMount() {
+  renderTable = () => {
+    return this.state.posts.map((post, idx) => (
+      <tr key={idx}>
+        <td>{post.title}</td>
+        <td>{post.body}</td>
+      </tr>
+    ));
   }
 
-  componentDidMount() {
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then(res => this.setState({data: { ...this.state.data, tempPosts: res.data } }))
-    // axios.get('https://jsonplaceholder.typicode.com/comments')
-    //   .then(res => this.setState({data: { ...this.state.data, comments: res.data } }))
+   getPosts(n) {
+      this.start = +new Date()
+      for (let i = 1; i <= n; i++) {
+        const id = Math.floor(Math.random() * 100) + 1;
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`).then(res => this.posts.push(res.data))
+      }
   }
 
-  renderTable() {
-    // const m = new jm(this.state.jsMeterOpt)
-    if (this.state.data.posts) {
-      return this.state.data.posts.map((row, idx) => (
-        <tr key={idx}>
-          <td>{row.title}</td>
-          <td>{row.body}</td>
-        </tr>
-      ));
+  async returnPosts(n) {
+    await this.getPosts(n);
+  }
+
+  async updatePosts(n) {
+    this.start = +new Date()
+
+    for (let i = 1; i <= n; i++) {
+      const id = Math.floor(Math.random() * 100) + 1;
+      await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`).then(res => this.posts[i] = res.data)
     }
-    // const meter = m.stop()
   }
 
   add100Rows() {
-    this.start = +new Date()
-    let tempRows = [...this.state.data.posts, ...this.state.data.tempPosts];
-    this.setState({ data: { ...this.state.data, posts: tempRows } })
+    this.start = +new Date();
+    // this.returnPosts(100)
+    this.getPosts(100)
+
+    // this.setState({posts: this.posts})
   }
 
   add1000Rows() {
-    this.start = +new Date()
-    let tempRows = [
-      ...this.state.data.posts,
-      ...this.state.data.tempPosts,
-      ...this.state.data.tempPosts,
-      ...this.state.data.tempPosts,
-      ...this.state.data.tempPosts,
-      ...this.state.data.tempPosts,
-      ...this.state.data.tempPosts,
-      ...this.state.data.tempPosts,
-      ...this.state.data.tempPosts,
-      ...this.state.data.tempPosts,
-      ...this.state.data.tempPosts,
-    ];
-    
-    this.setState({ data: { ...this.state.data, posts: tempRows } })
+    this.start = +new Date();
+    this.getPosts(1000).then(() => this.setState({posts: this.posts}));
   }
 
   clearRows() {
     this.start = +new Date()
-    this.setState({data: { ...this.state.data, posts: []}})
+    // this.setState({data: { ...this.state.data, posts: []}})
+    while(this.posts.length > 0) {
+      this.posts.pop()
+    }
+    this.setState({posts: this.posts})
   }
 
   updateEveryRow() {
     this.start = +new Date()
-    const updatedRows = this.state.data.posts.map(row => {
-      row.title += "!!!!"
-      row.body += "!!!!"
-      return row;
-    })
-    this.setState({ data: { ...this.state.data, posts: updatedRows } })
+    // const updatedRows = this.state.data.posts.map(row => {
+    //   row.title += "!!!!"
+    //   row.body += "!!!!"
+    //   return row;
+    // })
+    // this.setState({ data: { ...this.state.data, posts: updatedRows } })
+    this.updatePosts(this.state.posts.length).then(() => this.setState({posts: this.posts}))
   }
 
   swapRows() {
     this.start = +new Date()
-    const swapedRows = _.shuffle(this.state.data.posts)
-    this.setState({ data: { ...this.state.data, posts: swapedRows } })
+    // const swapedRows = _.shuffle(this.state.data.posts)
+    // this.setState({ data: { ...this.state.data, posts: swapedRows } })
+    if(this.state.posts.length >= 16){
+      const posts = this.state.posts;
+      var depotTitle = posts[1].title;
+      var depotBody = posts[1].body;
+      posts[1].title = posts[15].title;
+      posts[1].body = posts[15].body;
+      posts[15].title = depotTitle;
+      posts[15].body = depotBody;
+      this.setState({posts})
+    }
   }
 
   render() {
+    console.log(!!this.state.posts);
     return (
       <div>
         <button onClick={this.add100Rows}>Add 100 Rows</button>
@@ -114,7 +123,7 @@ class Table extends Component {
             <tr><td>Title</td><td>Body</td></tr>
           </thead>
           <tbody>
-            {this.renderTable()}
+            {this.state.posts && this.renderTable()}
           </tbody>
         </table>
       </div>
